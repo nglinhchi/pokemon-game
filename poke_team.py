f6fa997db1dad2a22cd3fd46a61b2bdc8ab7ed93
lngu0070 <87073420+nglinhchi@users.noreply.github.com>
2022-09-12 17:32:41 +1000

    choose_battle_option done

--- poke_team.py
+++ poke_team.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from multiprocessing.sharedctypes import Value
+from random import Random
 
 """
 """
@@ -24,11 +25,12 @@ class Criterion(Enum):
 class PokeTeam:
     MAX_TEAM_SIZE = 6
     NUM_BASE_POKEMON = 5
+    
     class AI(Enum):
-        ALWAYS_ATTACK = 0
-        SWAP_ON_SUPER_EFFECTIVE = 1
-        RANDOM = 2
-        USER_INPUT = 3
+        ALWAYS_ATTACK = auto()
+        SWAP_ON_SUPER_EFFECTIVE = auto()
+        RANDOM = auto()
+        USER_INPUT = auto()
 
 
     def __init__(self, team_name: str, team_numbers: list[int], battle_mode: int, ai_type: PokeTeam.AI, criterion=None, criterion_value=None) -> None:
@@ -77,11 +79,10 @@ class PokeTeam:
         self.battle_mode = battle_mode
         self.ai_type =  ai_type
         self.criterion = criterion
-        # self.criterion_value = criterion_value 
 
 
 
-
+    # TODO
     @classmethod
     def random_team(cls, team_name: str, battle_mode: int, team_size=None, ai_mode=None, **kwargs):
         """
@@ -91,24 +92,20 @@ class PokeTeam:
         if team_size == None:
             team_size = RandomGen.randint(PokeTeam.MAX_TEAM_SIZE//2, PokeTeam.MAX_TEAM_SIZE)
 
-        # create team_numbers TODO)switch to ADT
+        # create team_numbers TODO ADT and sort --------------------------------
         team_numbers = []
         team_numbers.append(0)
         team_numbers.append(team_size)
         for i in range(PokeTeam.NUM_BASE_POKEMON-2): 
             team_numbers.append(RandomGen.randint(0,team_size))
-        # TODO sort team_numbers
-
         if ai_mode == None:
             ai_mode = PokeTeam.AI.RANDOM
+        # ----------------------------------------------------------------------
 
         PokeTeam.__init__(team_name, team_numbers, battle_mode, ai_mode)
-
         
 
-        
-
-
+    # TODO
     def return_pokemon(self, poke: PokemonBase) -> None:
         if self.battle_mode == 0:
             pass
@@ -118,6 +115,7 @@ class PokeTeam:
             pass
 
 
+    # TODO
     def retrieve_pokemon(self) -> PokemonBase | None:
         if self.battle_mode == 0:
             pass
@@ -127,6 +125,7 @@ class PokeTeam:
             pass
 
 
+    # TODO
     def special(self):
         if self.battle_mode == 0:
             pass
@@ -135,32 +134,51 @@ class PokeTeam:
         elif self.battle_mode == 2:
             pass
 
-
+    
+    # TODO
     def regenerate_team(self):
         raise NotImplementedError()
 
 
+    # TODO
     def __str__(self):
         raise NotImplementedError()
 
-
+    # TODO
     def is_empty(self):
         raise NotImplementedError()
 
 
     def choose_battle_option(self, my_pokemon: PokemonBase, their_pokemon: PokemonBase) -> Action:
+        
         if self.battle_mode == PokeTeam.AI.ALWAYS_ATTACK:
             return Action.ATTACK
+        
         elif self.battle_mode == PokeTeam.AI.SWAP_ON_SUPER_EFFECTIVE:
             if their_pokemon.poke_type.type_multiplier(my_pokemon.poke_type) >= 1.5:
                 return Action.SWAP
             else:
                 return Action.ATTACK
+        
         elif self.battle_mode == PokeTeam.AI.RANDOM:
-            
+            return Action(Random.randint(1,4))
+        
         elif self.battle_mode == PokeTeam.AI.USER_INPUT:
-            pass
-
+            prompt = "Select an action: \n"
+            prompt += "1 - ATTACK \n"
+            prompt += "2 - SWAP \n"
+            prompt += "3 - HEAL \n"
+            prompt += "4 - SPECIAL \n"
+            prompt += "-----------------"
+            while True:
+                try:
+                    action = int(input(prompt))
+                    if not (action >=1 and action <= 4):
+                        raise ValueError("Invalid input. Please enter a number from 1 to 4.")
+                    break
+                except TypeError:
+                    print("Invalid input. Please enter a number")
+            return Action(action)
 
     @classmethod
     def leaderboard_team(cls):
