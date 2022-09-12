59e6070998e291a91be67045073d3352a845d1cc
Joong Do Chiang <jchi0099@student.monash.edu>
2022-09-12 16:34:06 +1000

    Poke_team init checks
    User facing args checks completed for init

--- poke_team.py
+++ poke_team.py
@@ -20,7 +20,8 @@ class Criterion(Enum):
     DEF = auto()
 
 class PokeTeam:
-
+    MAX_TEAM_SIZE = 6
+    NUM_BASE_POKEMON = 5
     class AI(Enum):
         ALWAYS_ATTACK = auto()
         SWAP_ON_SUPER_EFFECTIVE = auto()
@@ -30,7 +31,51 @@ class PokeTeam:
     def __init__(self, team_name: str, team_numbers: list[int], battle_mode: int, ai_type: PokeTeam.AI, criterion=None, criterion_value=None) -> None:
         """
         Creates user-specified Poketeam
-        """
+        """ 
+        ### User argument validation ###
+
+        #Check type(team_name) == str
+        if not type(team_name) == str:
+            raise TypeError("Team name must be string")
+
+        #Check team_numbers: len == 6, team_numbers[0] == 0, team_numbers[5] <= MAX_TEAM_SIZE, 0 <= team_numbers[1->4] <= team_numbers[5], check sorted == True
+
+        if not len(team_numbers) == PokeTeam.NUM_BASE_POKEMON + 1:  #Number of elements in list must equal number of base Pokemon + 1 for assignment of Pokemon to work
+            raise ValueError(f"Team number length is not valid. List must be of length: {PokeTeam.NUM_BASE_POKEMON + 1}")
+
+        for num in team_numbers:
+            if type(num) != int:
+                raise ValueError("Elements in list must be integers")
+        
+        if not team_numbers == sorted(team_numbers):
+            raise ValueError("Numbers must be sorted order (ascending)")
+
+        if team_numbers[0] != 0:
+            raise ValueError("First element in team numbers list must be 0")
+
+        elif team_numbers[-1] <= PokeTeam.MAX_TEAM_SIZE:
+            raise ValueError(f"Last element in team numbers list must be <= max team size: {PokeTeam.MAX_TEAM_SIZE}")
+        
+        if not (team_numbers[1] >= 0 and team_numbers[-2] <= team_numbers[-1]):   #Since we know from earlier check that list is sorted, only need to check second and second last element is valid as other element will be within that range.
+            raise ValueError(f"Numbers in list not within valid range: 0:{PokeTeam.MAX_TEAM_SIZE}")
+        
+        #Check battle_mode in [0,1,2]
+        if battle_mode not in [0,1,2]:
+            raise ValueError("Not valid Battle Mode")
+        #Check ai_type is valid (in PokeTeam.AI)
+        if ai_type not in PokeTeam.AI:
+            raise TypeError("AI Type is not valid")
+        #If battle_mode == 2, criterion provided and in Criterion class
+        if battle_mode == 2:
+            assert criterion != None, "Criterion for sorting must be provided for Battle Mode 2"
+            assert criterion in Criterion, f"{criterion} is not a valid criterion"
+            
+        ### End Checks ###
+
+        team_numbers 
+
+    
+
     
     @classmethod
     def random_team(cls, team_name: str, battle_mode: int, team_size=None, ai_mode=None, **kwargs):
