25363dc8893fb4545c1f31853ce76ddd9c7cac02
Joong Do Chiang <jchi0099@student.monash.edu>
2022-09-13 21:05:44 +1000

    Update Poke Team fix random size gen

--- poke_team.py
+++ poke_team.py
@@ -90,8 +90,13 @@ class PokeTeam:
         Creates random generated Poketeam
         """
         # assign team_size
+        if PokeTeam.MAX_TEAM_SIZE % 2 != 0:
+            half_team_max = PokeTeam.MAX_TEAM_SIZE // 2 + 1 #Between half of Pokemon limit and Pokemon limit- can't be less than half (floor division)
+        else:
+            half_team_max = PokeTeam.MAX_TEAM_SIZE//2
+
         if team_size == None:
-            team_size = RandomGen.randint(PokeTeam.MAX_TEAM_SIZE//2, PokeTeam.MAX_TEAM_SIZE)
+            team_size = RandomGen.randint(half_team_max, PokeTeam.MAX_TEAM_SIZE)
 
         # create team_numbers TODO ADT and sort --------------------------------
         team_numbers = []
