7819d57775c2bff7597e1a9bd00fcaf3a3a2c4de
jbut3196 <110904957+jbut3196@users.noreply.github.com>
2022-09-22 11:36:07 +1000

    Finish tests
    Finish all task 3 tests except __init__

--- tests/task3_tests.py
+++ tests/task3_tests.py
@@ -1,6 +1,7 @@
-from poke_team import *
+from poke_team import Action, Criterion, PokeTeam
 from random_gen import RandomGen
 from pokemon import *
+from pokemon_base *
 from tests.base_test import BaseTest
 
 class TestPokeTeam(BaseTest):
@@ -20,7 +21,7 @@ class TestPokeTeam(BaseTest):
         self.assertEqual(t.is_empty(), True)
         t = PokeTeam("team", [2, 0, 0, 1, 1], 1, PokeTeam.AI.RANDOM)
         self.assertEqual(t.is_empty(), False)
-        t = PokeTeam("team", [4, 0, 0, , 0], 1, PokeTeam.AI.RANDOM)
+        t = PokeTeam("team", [4, 0, 0, 0, 0], 1, PokeTeam.AI.RANDOM)
         self.assertEqual(t.is_empty(), False)
 
     def test_retrieve_pokemon(self):
@@ -47,10 +48,10 @@ class TestPokeTeam(BaseTest):
             self.assertIsInstance(p, e)
 
         # test battle mode 2 retrieves first pokemon in team
-        t = PokeTeam("team2", [1, 1, 0, 0, 2], 2, PokeTeam.AI.RANDOM)
+        t = PokeTeam("team2", [1, 1, 0, 0, 2], 2, PokeTeam.AI.RANDOM, Criterion.HP)
         t.retrieve_pokemon()
-        self.assertIsInstance(t.poke_on_field, Charmander)
-        expected_classes = [Eevee, Eevee, Bulbasaur]
+        self.assertIsInstance(t.poke_on_field, Bulbasaur)
+        expected_classes = [Eevee, Eevee, Charmander]
         pokemon = []
         while not t.is_empty():
             pokemon.append(t.retrieve_pokemon())
@@ -98,11 +99,109 @@ class TestPokeTeam(BaseTest):
         for p, e in zip(pokemon, expected_classes):
             self.assertIsInstance(p, e)
 
-
     def test_special(self):
+        #test battle mode 0 special swaps the first and last pokemon
+        t = PokeTeam("team0", [1, 1, 0, 0, 2], 0, PokeTeam.AI.RANDOM)
+        t.special()
+        expected_classes = [Eevee, Bulbasaur, Eevee, Charmander]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
+
+        #test battle mode 1 special swaps the first and second halves of the team, reversing the order of the previously front half
+        t = PokeTeam("team1", [1, 1, 0, 1, 2], 1, PokeTeam.AI.RANDOM)
+        t.special()
+        expected_classes = [Gastly, Eevee, Eevee, Bulbasaur, Charmander]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
+
+        #test battle mode 2 special reverses the sorting order of the team
+        t = PokeTeam("team2", [1, 1, 0, 0, 2], 2, PokeTeam.AI.RANDOM, Criterion.HP)
+        t.special()
+        expected_classes = [Charmander, Eevee, Eevee, Bulbasaur]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
 
     def test_regenerate_team(self):
+        #test battle mode 0 regenerates team
+        t = PokeTeam("team0", [1, 1, 0, 0, 2], 0, PokeTeam.AI.RANDOM)
+        while not t.is_empty():             #all pokemon will eventually faint
+            p = t.retrieve_pokemon()
+            p.lose_hp(3)
+            t.return_pokemon(p)
+        t.regenerate_team()
+        expected_classes = [Charmander, Bulbasaur, Eevee, Eevee]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
+
+        #test battle mode 1 regenerates team
+        t = PokeTeam("team1", [1, 1, 0, 0, 2], 1, PokeTeam.AI.RANDOM)
+        while not t.is_empty():             #all pokemon will eventually faint
+            p = t.retrieve_pokemon()
+            p.lose_hp(3)
+            t.return_pokemon(p)
+        t.regenerate_team()
+        expected_classes = [Charmander, Bulbasaur, Eevee, Eevee]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
+
+        #test battle mode 2 regenerates team
+        t = PokeTeam("team2", [1, 1, 0, 0, 2], 2, PokeTeam.AI.RANDOM, Criterion.HP)
+        while not t.is_empty():             #all pokemon will eventually faint
+            p = t.retrieve_pokemon()
+            p.lose_hp(3)
+            t.return_pokemon(p)
+        t.regenerate_team()
+        expected_classes = [Bulbasaur, Eevee, Eevee, Charmander]
+        pokemon = []
+        while not t.is_empty():
+            pokemon.append(t.retrieve_pokemon())
+        for p, e in zip(pokemon, expected_classes):
+            self.assertIsInstance(p, e)
 
     def test_choose_battle_option(self):
+        #test ALWAYS_ATTACK returns ATTACK
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 0, PokeTeam.AI.ALWAYS_ATTACK)
+        p = t.retrieve_pokemon()
+        b = Bulbasaur()
+        self.assertEqual(t.choose_battle_option(p, b), Action.ATTACK)
+
+        #test SWAP_ON_SUPER_EFFECTIVE when type multiplier < 1.5
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 1, PokeTeam.AI.SWAP_ON_SUPER_EFFECTIVE)
+        p = t.retrieve_pokemon()
+        b = Bulbasaur()
+        self.assertEqual(t.choose_battle_option(p, b), Action.ATTACK)
+
+        #test SWAP_ON_SUPER_EFFECTIVE when type multiplier > 1.5
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 1, PokeTeam.AI.SWAP_ON_SUPER_EFFECTIVE, Criterion.HP)
+        p = t.retrieve_pokemon()
+        c = Charmander
+        self.assertEqual(t.choose_battle_option(p, c), Action.SWAP)
 
     def test_str(self):
+        #test battle mode 0 prints the correct string
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 0, PokeTeam.AI.RANDOM)
+        self.assertEqual(str(t), "Bob (0): [LV. 1 Charmander: 9 HP, LV. 1 Bulbasaur: 13 HP, LV. 1 Eevee: 10 HP, LV. 1 Eevee: 10 HP]")
+
+        #test battle mode 1 prints the correct string
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 1, PokeTeam.AI.RANDOM)
+        self.assertEqual(str(t), "Bob (1): [LV. 1 Charmander: 9 HP, LV. 1 Bulbasaur: 13 HP, LV. 1 Eevee: 10 HP, LV. 1 Eevee: 10 HP]")
+
+        #test battle mode 2 prints the correct string
+        t = PokeTeam("Bob", [1, 1, 0, 0, 2], 1, PokeTeam.AI.RANDOM, Criterion.HP)
+        self.assertEqual(str(t), "Dawn (2): [LV. 1 Bulbasaur: 13 HP, LV. 1 Eevee: 10 HP, LV. 1 Eevee: 10 HP, LV. 1 Charmander: 9 HP]")
+
