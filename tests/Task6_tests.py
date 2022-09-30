d7930af0779e25c795a72077551e6980a62406c4
jbut3196 <110904957+jbut3196@users.noreply.github.com>
2022-09-30 23:29:31 +1000

    Start Task 6 Tests

---
+++ tests/Task6_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from pokemon_base import *
+from pokemon import *
+from base_test import *
+from battle import *
+from tournament import *
+from unittest.mock import patch
+
+class TestBattle(BaseTest):
+    def test_is_valid_tournament(self):
+        t = Tournament(Battle(verbosity=0))
+        t.set_battle_mode(1)
+        self.assertEqual(t.is_valid_tournament(Monfils Berrettini + Shapovalov Nadal + + Sinner Tsitsipas + Auger-Aliassime Medvedev + + +), True)
+
+        t = Tournament(Battle(verbosity=0))
+        t.set_battle_mode(1)
+        self.assertEqual(t.is_valid_tournament(t1 t2 + t3 + +), False)
+
+        t = Tournament(Battle(verbosity=0))
+        t.set_battle_mode(1)
+        self.assertEqual(t.is_valid_tournament(1234), False)
