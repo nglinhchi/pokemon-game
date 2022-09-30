ebd6177a0b50682e0785ba377373193c086804a9
lngu0070 <87073420+nglinhchi@users.noreply.github.com>
2022-09-30 22:56:00 +1000

    update battle to meet req for order of handling actions

--- battle.py
+++ battle.py
@@ -12,6 +12,7 @@ from print_screen import *
 import os, sys
 
 class HiddenPrints:
+    
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
@@ -19,12 +20,11 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+
 class Battle:
     
     def __init__(self, verbosity=0) -> None:
         self.verbosity = verbosity
-
-
 
     def battle(self, team1: PokeTeam, team2: PokeTeam) -> int:
         # with HiddenPrints():
@@ -48,7 +48,12 @@ class Battle:
                     if pokemon2 == None: 
                         pokemon2 = team2.retrieve_pokemon()
                     
-                    # print_game_screen(pokemon1.get_name(), pokemon2.get_name(), pokemon1.get_hp(), pokemon1.get_max_hp(), pokemon2.get_hp(), pokemon2.get_max_hp(), pokemon1.get_level(), pokemon2.get_level(), pokemon1.get_status_effect(), pokemon2.get_status_effect(), len(team1.team), len(team2.team))
+                    # print_game_screen(pokemon1.get_name(), pokemon2.get_name(), 
+                    #                   pokemon1.get_hp(), pokemon1.get_max_hp(), pokemon2.get_hp(), pokemon2.get_max_hp(), 
+                    #                   pokemon1.get_level(), pokemon2.get_level(), 
+                    #                   pokemon1.get_status_effect(), pokemon2.get_status_effect(), 
+                    #                   len(team1.team), len(team2.team))
+
                     # battle here -----------------------------------------------------------------
                     # team1.poke_on_field = pokemon1
                     # team2.poke_on_field = pokemon2
@@ -67,14 +72,27 @@ class Battle:
                         team1.return_pokemon(pokemon1)
                         print("after swap return", team1.team)
                         pokemon1 = team1.retrieve_pokemon()
-                    elif action1 == Action.SPECIAL:
+
+                    if action2 == Action.SWAP:
+                        print("swapping2")
+                        team2.return_pokemon(pokemon2)
+                        pokemon2 = team2.retrieve_pokemon()
+
+                    if action1 == Action.SPECIAL:
                         print("poke prior to speci 1-2", pokemon1, pokemon2)
                         print("special")
                         print("descending?", team1.descending_order)
                         team1.return_pokemon(pokemon1) #Should be implemented inside special
                         team1.special()
                         pokemon1 = team1.retrieve_pokemon()
-                    elif action1 == Action.HEAL:
+
+                    if action2 == Action.SPECIAL:
+                        print("special2")
+                        team2.return_pokemon(pokemon2)
+                        team2.special()
+                        pokemon2 = team2.retrieve_pokemon()
+
+                    if action1 == Action.HEAL:
                         print("heal")
                         if team1.heal_count < 0:
                             return 2
@@ -82,19 +100,7 @@ class Battle:
                             team1.heal_count -= 1
                             pokemon1.heal()
 
-                    # TEAM 2 PRE-ATTACK -------------------------------------------------------------------
-
-                    if action2 == Action.SWAP:
-                        print("swapping2")
-                        team2.return_pokemon(pokemon2)
-                        pokemon2 = team2.retrieve_pokemon()
-                    elif action2 == Action.SPECIAL:
-                        print("special2")
-                        team2.return_pokemon(pokemon2)
-
-                        team2.special()
-                        pokemon2 = team2.retrieve_pokemon()
-                    elif action2 == Action.HEAL:
+                    if action2 == Action.HEAL:
                         print("heal2")
                         if team2.heal_count < 0:
                             return 1
@@ -103,6 +109,7 @@ class Battle:
                             pokemon2.heal()
                     print("teamstate1", team1, team1.team)
                     print("teamstate2", team2)
+                    
                     # TEAM 1 AND 2 ATTACKS -------------------------------------------------------------------
 
                     if action1 == Action.ATTACK and action2 == Action.ATTACK: # both attacks
@@ -135,38 +142,30 @@ class Battle:
 
                     # battle ends here ------------------------------------------------------------
                     print("before post battle", pokemon1, pokemon2, "status effects", pokemon1.get_status_effect(), pokemon2.get_status_effect())
+                    
+                    # both pokemons are not fainted --> both lose 1 hp
                     if (not pokemon1.is_fainted()) and (not pokemon2.is_fainted()): #should not be elif next because the lose hp could make a pokemon faint, resulting in other one level up
                         pokemon1.lose_hp(1)
                         pokemon2.lose_hp(1)
                     
+                    # one pokemon is fainted, the other is not -> surviving pokemon level up
                     if (not pokemon1.is_fainted()) and pokemon2.is_fainted():
                         pokemon1.level_up()
                     elif pokemon1.is_fainted() and (not pokemon2.is_fainted()):
                         pokemon2.level_up()
-                    # print(pokemon1.is_fainted(), pokemon2.is_fainted())
                     
+                    # if any surviving pokemon can and should evolve -> it evolves
                     if (not pokemon1.is_fainted()) and pokemon1.can_evolve() and pokemon1.should_evolve():
                         pokemon1 = pokemon1.get_evolved_version()
                         team1.poke_on_field = pokemon1
-                    
                     if (not pokemon2.is_fainted()) and pokemon2.can_evolve() and pokemon2.should_evolve():
                         pokemon2 = pokemon2.get_evolved_version()
                         team2.poke_on_field = pokemon2
 
+                    # remove any fainted pokemons
                     if pokemon1.is_fainted():
                         pokemon1 = None
                     if pokemon2.is_fainted():
                         pokemon2 = None
+
                     print(pokemon1, pokemon2)
-                
-
-
-if __name__ == "__main__":
-    b = Battle(verbosity=3)
-    RandomGen.set_seed(16)
-    t1 = PokeTeam.random_team("Cynthia", 0, criterion=Criterion.SPD)
-    
-    t1.ai_type = PokeTeam.AI.USER_INPUT
-    t2 = PokeTeam.random_team("Barry", 1)
-    print(b.battle(t1, t2))
-
